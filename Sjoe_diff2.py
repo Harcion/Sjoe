aaf201253272b23599f0557228623d8c7e2466eb
Tony Stillfjord <tony@BEAST.(none)>
2009-08-13 10:52:05 +0200

    Changed structure of jacobian calculation.

--- Sjoe_diff2.py
+++ Sjoe_diff2.py
@@ -27,7 +27,6 @@ def jac(f, x):
 	return A
 
 def f(u):
-	#z = u[:-5*n]
 	z = u[:ind.xdbldot()]
 	dz = oderhs(z,0)
 	fu = array(zeros(2+3*n))
@@ -37,17 +36,17 @@ def f(u):
 
 
 ## Initial position/motion of the rolling elements
-n = 1
+n = 4
 N = 5*n+4
 u0 = array(zeros(N))
 ind = Index(n)
 
 for i in xrange(0,n):
-	u0[ind.r(i)] 		= (b0+c0)/2       # r
-	u0[ind.theta(i)] 	= 2*pi*i/n        # theta
-	u0[ind.rdot(i)] 	= 0               # rdot
-	u0[ind.thetadot(i)] = b0/(b0+c0)*w0   # thetadot
-	u0[ind.phidot(i)] 	= b0/2/a0*w0      # phidot - note that phi is not a state variable since it does not matter here
+	u0[ind.r(i)] 		= (b0+c0)/2			# r
+	u0[ind.theta(i)] 	= -pi/2+2*pi*i/n	# theta
+	u0[ind.rdot(i)] 	= 0             	# rdot
+	u0[ind.thetadot(i)] = b0/(b0+c0)*w0 	# thetadot
+	u0[ind.phidot(i)] 	= b0/2/a0*w0    	# phidot - note that phi is not a state variable since it does not matter here
 
 u0_ = r_[u0, array(zeros(2+3*n))]
 print u0_
@@ -55,69 +54,82 @@ print f(u0_)
 print ""
 
 J = jac(f, u0_)
-print J
+#print J
 
 (M,N) = shape(J)
 
 Js = array(zeros(J.shape))
 #exc = array([ind.xdot(), ind.ydot(), ind.xdbldot(), ind.ydbldot()])
-exc = array([ind.x(), ind.y(), ind.xdot(), ind.ydot()])
+#exc = array([ind.x(), ind.y(), ind.xdot(), ind.ydot()])
+exc = array([ind.xdot(), ind.ydot(), ind.xdbldot(), ind.ydbldot()])
+q = array([ind.x(), ind.y()])
+
 for i in range(0,n):
 	#exc = r_[exc, array([ind.rdot(i), ind.thetadot(i), ind.rdbldot(i), ind.thetadbldot(i), ind.phidbldot(i)])]
-	exc = r_[exc, array([ind.r(i), ind.theta(i), ind.rdot(i), ind.thetadot(i), ind.phidot(i)])]
+	#exc = r_[exc, array([ind.r(i), ind.theta(i), ind.rdot(i), ind.thetadot(i), ind.phidot(i)])]
+	exc = r_[exc, array([ind.rdot(i), ind.thetadbldot(i), ind.phidbldot(i)])]
+	q = r_[q, array([ind.r(i), ind.theta(i), ind.thetadot(i), ind.phidot(i), ind.rdbldot(i)])]
+	
 k = 0
+#for i in set(range(0,N)) - set(exc):
+#	Js[:,k] = copy(J[:,i])
+#	k += 1
+#
+#for i in exc:
+#	Js[:,k] = copy(J[:,i])
+#	k += 1
 
-for i in set(range(0,N)) - set(exc):
-	print i
+for i in q:
 	Js[:,k] = copy(J[:,i])
 	k += 1
 
-for i in exc:
+for i in set(range(0,N)) - set(q):
 	Js[:,k] = copy(J[:,i])
 	k += 1
 
-P  = Js[:5*n,:7*n]
-print P
-print pylab.rank(P)
 
-(MP, NP) = P.shape
-(U, s, Vh) = sc.linalg.svd(P)
-S = sc.linalg.diagsvd(s, MP, NP)
 
-print U
-print ""
-print S
-print ""
-print Vh
+#P  = Js[:,:2+3*n]
+P  = Js[:,:q.size]  #(N-exc.size)
+print P.shape
+#print P
+prank = pylab.rank(P)
+print "Rank:", prank
+max_rank = 2+3*n
+print "Max rank:", max_rank
+print "Js rank:", pylab.rank(Js)
 
-print pylab.rank(S)
-maxabs = np.max(np.absolute(s))
-maxdim = max(P.shape)
-
-tol    = maxabs * maxdim * 1e-16#mlab._eps_approx
-print "maxabs", maxabs
-print "maxdim", maxdim
-print "tol", tol
-print npy.sum(s > tol)
+#(MP, NP) = P.shape
+#(U, s, Vh) = sc.linalg.svd(P)
+#S = sc.linalg.diagsvd(s, MP, NP)
+#
+#print U
+#print ""
+#print S
+#print ""
+#print Vh
+#
+#print pylab.rank(S)
 
 #
-#def permute(P, frm, to):
-#	(M,N) = P.shape
-#	if (frm == to) or (frm == N + to) or (to == frm + N):  # Don't need to permute anything
-#		print frm
-#		print to
-#		return P
-#	P[frm, to]  = 1
-#	P[to, frm]  = 1
-#	P[frm,frm] = 0
-#	P[to,to] = 0
-#	return P
-#
-#P = diag(ones(N)) # Permutation matrix
-#P = permute(P, 2+2*n, -1) # xdot
-#P = permute(P, 3+2*n, -2) # ydot
-#P = permute(P, 4+5*n, -3) # xdoubledot
-#P = permute(P, 5+5*n, -4) # ydoubledot
-#P = permute(P, 4+2*n, -5) # rdot
-#P = permute(P, 4+5*n + 2, -6) # thetadoubledot
-#P = permute(P, 4+5*n + 3, -7) # phidoubledot
+#for i in range(0,N):
+#	P2 = c_[P , Js[:,i]]
+#	if pylab.rank(P2) > prank:
+#		print i
+#		for k in range(0,N):
+#			P3 = c_[P2 , Js[:,k]]
+#			if pylab.rank(P3) == max_rank:
+#				print "   ", k
+
+
+#for i in range(0,max_rank):
+#	P3 = copy(P)
+#	P3 = delete(P3, i, axis=1) # delete ith column
+#	if pylab.rank(P3) == prank:
+#		print i
+		
+#Js[:, ind.phidot(0)],  Js[:, ind.rdbldot(3)], Js[:, ind.rdbldot(1)],
+#for i in range(0,N):
+#	P3 = c_[P2 , Js[:,i]]
+#	if pylab.rank(P3) == max_rank:
+#		print i
